92ebe2d69a607222ccad96b221e5bcb2bd02eb40
Alejo Flores Lucey <alejo@misterflores.com>
2022-09-28 16:30:21 -0300

    Finished Routine Card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,16 @@
   <div class="home">
 <!--    <img alt="Vue logo" src="../assets/logo.png" />-->
     <h1 class="primary">Texto de prueba</h1>
+    <RoutineCard/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import RoutineCard from "@/components/RoutineCard";
 export default {
   name: "HomeView",
+  components: {RoutineCard},
 };
 </script>
 
