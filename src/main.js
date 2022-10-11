6d381a3e290799bcbb10ce0e3326977d736657e6
NehuenLlanos <nehuengllanos@gmail.com>
2022-10-10 23:35:16 -0300

    Dependencies problems seems to be fixed

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 import { createPinia, PiniaVuePlugin } from 'pinia'
-import VueCompositionAPI from '@vue/composition-api'
 import Vuelidate from 'vuelidate'
 
 Vue.use(PiniaVuePlugin)
 const pinia = createPinia()
-Vue.use(VueCompositionAPI)
 Vue.use(Vuelidate)
 
 Vue.config.productionTip = false;
