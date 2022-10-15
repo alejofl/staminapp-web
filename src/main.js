b7c915d4fc4b7c77c922ac55ff5e7390f6eb0d25
NehuenLlanos <nehuengllanos@gmail.com>
2022-10-15 16:21:36 -0300

    Done.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import pinia from "./plugins/pinia";
 import router from "./router";
-import { createPinia, PiniaVuePlugin } from 'pinia'
 import Vuelidate from 'vuelidate'
 
-Vue.use(PiniaVuePlugin)
-const pinia = createPinia()
+
 Vue.use(Vuelidate)
 
 Vue.config.productionTip = false;
+
+
 
 new Vue({
   vuetify,
